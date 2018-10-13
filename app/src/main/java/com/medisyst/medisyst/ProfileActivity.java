91a697ce2acdcc13c13b70e8606d16de090e082d
Ritik Srivastava <ritik.space@gmail.com>
2018-10-13 19:09:30 +0530

    Camera UI Development

--- app/src/main/java/com/medisyst/medisyst/ProfileActivity.java
+++ app/src/main/java/com/medisyst/medisyst/ProfileActivity.java
@@ -188,8 +188,10 @@ public class ProfileActivity extends AppCompatActivity {
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                startActivity(intent);
+//                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+//                startActivity(intent);
+                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                getWindow().setStatusBarColor(Color.TRANSPARENT);
                 if(profile_lp) {profile_lp=false;}
                 else
                 {

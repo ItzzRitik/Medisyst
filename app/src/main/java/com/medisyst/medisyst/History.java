acf8293acbafe6bd75c0f7f0b9f559c3bf71732f
Ritik Srivastava <ritik.space@gmail.com>
2018-10-13 22:05:54 +0530

    Home Development - History

--- app/src/main/java/com/medisyst/medisyst/History.java
+++ app/src/main/java/com/medisyst/medisyst/History.java
@@ -1,15 +1,18 @@
 package com.medisyst.medisyst;
 public class History {
-    private String name,date,pName,solution
-    History(String name, String date, String dob,String pName,String solution) {
+    private String name,date,profName,solution;
+    History(String name, String date, String dob,String profName,String solution) {
         this.name = name;
         this.date = date;
-        this.dob = dob;
+        this.profName = profName;
+        this.solution = solution;
     }
     public String getName() {return name;}
     public void setName(String name) {this.name = name;}
     public String getDate() {return date;}
-    public void setDate(String gender) {this.date = date;}
-    public String getDob() {return dob;}
-    public void setDob(String dob) {this.dob = dob;}
+    public void setDate(String date) {this.date = date;}
+    public String getProfName() {return profName;}
+    public void setProfName(String profName) {this.profName = profName;}
+    public String getSolution() {return solution;}
+    public void setSolution(String solution) {this.solution = solution;}
 }

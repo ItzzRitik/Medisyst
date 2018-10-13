55017513d9ea9523b0160d3214bd40b2e4b6e76c
Ritik Srivastava <ritik.space@gmail.com>
2018-10-14 03:14:26 +0530

    HomePage History Adapter

--- app/src/main/java/com/medisyst/medisyst/History.java
+++ app/src/main/java/com/medisyst/medisyst/History.java
@@ -1,9 +1,10 @@
 package com.medisyst.medisyst;
 public class History {
-    private String name,date,profName,solution;
-    History(String name, String date, String dob,String profName,String solution) {
+    private String name,date,profName,docName,solution;
+    History(String name, String date,String profName,String docName,String solution) {
         this.name = name;
         this.date = date;
+        this.docName = docName;
         this.profName = profName;
         this.solution = solution;
     }
@@ -11,6 +12,8 @@ public class History {
     public void setName(String name) {this.name = name;}
     public String getDate() {return date;}
     public void setDate(String date) {this.date = date;}
+    public String getdocName() {return docName;}
+    public void setdocName(String date) {this.docName = docName;}
     public String getProfName() {return profName;}
     public void setProfName(String profName) {this.profName = profName;}
     public String getSolution() {return solution;}

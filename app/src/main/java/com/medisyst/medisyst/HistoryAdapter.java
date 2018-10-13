a36047510d215a3ed03b2f8da0257ec11384e419
Ritik Srivastava <ritik.space@gmail.com>
2018-10-13 22:47:18 +0530

    Home Development - History Cards

--- app/src/main/java/com/medisyst/medisyst/HistoryAdapter.java
+++ app/src/main/java/com/medisyst/medisyst/HistoryAdapter.java
@@ -18,33 +18,44 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.MyViewHolder> {
-    private List<History> schemes;
-    String email="",aadhaar="";
+    private List<History> history;
     private HomeActivity homeActivity;
+    private int[] thumb={R.drawable.h1,R.drawable.h2,R.drawable.h3,R.drawable.h4,R.drawable.h5,R.drawable.h6,R.drawable.h7,R.drawable.h8};
     class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView title,last_date,views;
+        TextView name,date,prof_name,solution;
         LinearLayout cardItem;
         ImageView thumbnail;
         MyViewHolder(View view) {
             super(view);
-            title = view.findViewById(R.id.title);
-            title.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
-            views = view.findViewById(R.id.views);
-            views.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
-            last_date = view.findViewById(R.id.date);
-            last_date.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
+            name = view.findViewById(R.id.title);
+            name.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
+            date = view.findViewById(R.id.views);
+            date.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
+            prof_name = view.findViewById(R.id.date);
+            prof_name.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
+            solution = view.findViewById(R.id.date);
+            solution.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
             thumbnail = view.findViewById(R.id.thumbnail);
             cardItem = view.findViewById(R.id.cardItem);
         }
     }
-    HistoryAdapter(HomeActivity homeActivity, List<History> schemes, String email, String aadhaar) {
-        this.schemes = schemes;
+    HistoryAdapter(HomeActivity homeActivity, List<History> history) {
+        this.history = history;
         this.homeActivity = homeActivity;
-        this.email=email;
-        this.aadhaar=aadhaar;
+
+        Random rnd = ThreadLocalRandom.current();
+        for (int i=thumb.length-1;i>0;i--)
+        {
+            int index = rnd.nextInt(i+1);
+            int a = thumb[index];
+            thumb[index] = thumb[i];
+            thumb[i] = a;
+        }
     }
     @NonNull
     @Override

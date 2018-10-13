956f9c9a94882b88c2cd3d29a1924593ed85b2b7
Ritik Srivastava <ritik.space@gmail.com>
2018-10-13 22:51:05 +0530

    Home Development - History Cards

--- app/src/main/java/com/medisyst/medisyst/HistoryAdapter.java
+++ app/src/main/java/com/medisyst/medisyst/HistoryAdapter.java
@@ -1,26 +1,16 @@
 package com.medisyst.medisyst;
-import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import com.squareup.picasso.Picasso;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 
 public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.MyViewHolder> {
     private List<History> history;
@@ -32,13 +22,13 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.MyViewHo
         ImageView thumbnail;
         MyViewHolder(View view) {
             super(view);
-            name = view.findViewById(R.id.title);
+            name = view.findViewById(R.id.name);
             name.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
-            date = view.findViewById(R.id.views);
+            date = view.findViewById(R.id.date);
             date.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
-            prof_name = view.findViewById(R.id.date);
+            prof_name = view.findViewById(R.id.prof_name);
             prof_name.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
-            solution = view.findViewById(R.id.date);
+            solution = view.findViewById(R.id.solution);
             solution.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
             thumbnail = view.findViewById(R.id.thumbnail);
             cardItem = view.findViewById(R.id.cardItem);
@@ -60,49 +50,26 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.MyViewHo
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_card, parent, false);
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_people, parent, false);
         return new MyViewHolder(itemView);
     }
     @Override
     public void onBindViewHolder(@NonNull final MyViewHolder holder, int position) {
-        final Schemes scheme = schemes.get(position);
-        holder.title.setText(scheme.getTitle());
-        holder.last_date.setText(daysLeft(scheme.getDate()));
-        holder.views.setText(scheme.getViews());
-        Picasso.get().load(scheme.getThumbnail()).into(holder.thumbnail);
+        final History item = history.get(position);
+        holder.name.setText(item.getName());
+        holder.date.setText(item.getDate());
+        holder.prof_name.setText(item.getProfName());
+        holder.solution.setText(item.getSolution());
 
         holder.cardItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent home=new Intent(homeActivity,SchemeActivity.class);
-                Log.i("email",email);
-                home.putExtra("email",email);
-                home.putExtra("aadhaar",aadhaar);
-                home.putExtra("name",scheme.getTitle());
-                home.putExtra("date",scheme.getDate());
-                home.putExtra("days",daysLeft(scheme.getDate()));
-                home.putExtra("details",scheme.getDetails());
-                home.putExtra("img",scheme.getThumbnail());
-                homeActivity.startActivity(home);
-                homeActivity.overridePendingTransition(R.anim.fade_in,0);
+
             }
         });
     }
     @Override
     public int getItemCount() {
-        return schemes.size();
-    }
-    private String daysLeft(String date)
-    {
-        long diffInMillies=0;
-        try{
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
-            Calendar c = Calendar.getInstance();
-            Date firstDate = sdf.parse(sdf.format(c.getTime()));
-            Date secondDate = sdf.parse(date);
-            diffInMillies = Math.abs(secondDate.getTime() - firstDate.getTime());
-        }
-        catch (Exception ignored){}
-        return((TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS)+1)+" Days\nRemaining");
+        return history.size();
     }
 }

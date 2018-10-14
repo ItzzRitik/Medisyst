bc748b0494ceeaa9bf2d766cd2ff3d588541beac
Ritik Srivastava <ritik.space@gmail.com>
2018-10-14 06:46:37 +0530

    HomePage History Implementation

---
+++ app/src/main/java/com/medisyst/medisyst/Permission.java
@@ -0,0 +1,12 @@
+package com.medisyst.medisyst;
+public class Permission {
+    private String name,key;
+    Permission(String name, String key) {
+        this.name = name;
+        this.key = key;
+    }
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public String getKey() {return key;}
+    public void setKey(String key) {this.key = key;}
+}
--- app/src/main/java/com/medisyst/medisyst/PermissionAdapter.java
+++ app/src/main/java/com/medisyst/medisyst/PermissionAdapter.java
@@ -14,52 +14,53 @@ import java.util.List;
 import java.util.Random;
 
 public class PermissionAdapter extends RecyclerView.Adapter<PermissionAdapter.MyViewHolder> {
-    private List<History> history;
+    private List<Permission> permission;
     private HomeActivity homeActivity;
     private Random random;
     private int[] thumb={R.drawable.h1,R.drawable.h2,R.drawable.h3,R.drawable.h4,
             R.drawable.h5,R.drawable.h6,R.drawable.h7,R.drawable.h8};
     class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView name,date,prof_name,doc_name,solution;
+        TextView name,key,accept,reject;
         LinearLayout cardItem;
         ImageView thumbnail;
         MyViewHolder(View view) {
             super(view);
             name = view.findViewById(R.id.name);
             name.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2_bold.otf"));
-            date = view.findViewById(R.id.date);
-            date.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
-            prof_name = view.findViewById(R.id.prof_name);
-            prof_name.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
-            solution = view.findViewById(R.id.solution);
-            solution.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
-            doc_name= view.findViewById(R.id.doc_name);
-            doc_name.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
+            key = view.findViewById(R.id.key);
+            key.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
+            accept = view.findViewById(R.id.accept);
+            accept.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
+            reject = view.findViewById(R.id.reject);
+            reject.setTypeface(Typeface.createFromAsset(homeActivity.getAssets(), "fonts/exo2.ttf"));
             thumbnail = view.findViewById(R.id.thumbnail);
             cardItem = view.findViewById(R.id.cardItem);
         }
     }
-    PermissionAdapter(HomeActivity homeActivity, List<History> history) {
-        this.history = history;
+    PermissionAdapter(HomeActivity homeActivity, List<Permission> Permission) {
+        this.permission = Permission;
         this.homeActivity = homeActivity;
         random = new Random();
     }
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_history, parent, false);
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_permission, parent, false);
         return new MyViewHolder(itemView);
     }
     @Override
     public void onBindViewHolder(@NonNull final MyViewHolder holder, int position) {
-        final History item = history.get(position);
+        final Permission item = permission.get(position);
         holder.thumbnail.setImageDrawable(homeActivity.getDrawable(thumb[random.nextInt(thumb.length-1)]));
         holder.name.setText(item.getName());
-        holder.date.setText(item.getDate());
-        holder.prof_name.setText(item.getProfName());
-        holder.solution.setText(item.getSolution());
-        holder.doc_name.setText(item.getdocName());
-        holder.cardItem.setOnClickListener(new View.OnClickListener() {
+        holder.key.setText(item.getKey());
+        holder.accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        holder.reject.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             }
@@ -67,6 +68,6 @@ public class PermissionAdapter extends RecyclerView.Adapter<PermissionAdapter.My
     }
     @Override
     public int getItemCount() {
-        return history.size();
+        return permission.size();
     }
 }
